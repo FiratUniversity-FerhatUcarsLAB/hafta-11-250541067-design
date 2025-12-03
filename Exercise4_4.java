ff90b9d4f0954c074e32c92fff1aeba869ee261c
250541067-design <250541067@firat.edu.tr>
2025-12-03 21:26:00 +0300

    Exercise4_4

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -1,21 +1,33 @@
 public class Exercise4_4 {
 
-    public static int getNumber() {
+    public static int giveNumber() {
         return 42;
     }
 
-    public static void sayHello() {
-        System.out.println("hello");
+    public static void sayBoo() {
+        System.out.println("boo!");
     }
 
     public static void main(String[] args) {
 
-        // 1) Donus degerini kullanmayin -> ne oluyor?
-        getNumber();
+        // 1) Bir deger donduren metodu cagirip sonucunu KULLANMAZSAN ne olur?
+        // --------------------------------------------------------------------
+        // Java DERLEME HATASI vermez.
+        // Metot cagirilir, dondurdugu deger kaybolur ama bu bir hata degildir.
+        // Sadece IDE "unused return value" uyarisi verebilir.
 
-        // 2) void metodu ifade icinde kullanin -> ne oluyor?
-        // System.out.println(sayHello() + 7);
+        giveNumber();   // (Bu satir derleme hatasi OLUSTURMAZ)
 
-        // Cevaplarinizi yorum olarak ekleyin.
+
+        // 2) Bir void metodu bir ifadenin icinde kullanirsan ne olur?
+        // --------------------------------------------------------------------
+        // Void metod DEGER dondurmez.
+        // Bu nedenle bir ifadeye (expression) yerlesitirilemez.
+        // Aksi durumda DERLEME HATASI olusur:
+        // "void type not allowed here" veya "void cannot be used in expression".
+
+        // System.out.println(sayBoo() + 7);
+        // YUKARIDAKI SATIR DERLEME HATASI VERIR.
+
     }
 }
