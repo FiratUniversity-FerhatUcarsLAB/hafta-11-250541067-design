8ebd874bf4e188d14fde8b3af67947b0ac8aee90
250541067-design <250541067@firat.edu.tr>
2025-12-03 15:53:05 +0300

    4.1 egzersiz
    Implemented methods to print dates in American and European formats.

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,23 @@
 public class Exercise4_1 {
 
     public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+        System.out.println(day + ", " + month + " " + date + ", " + year);
     }
 
     public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+         System.out.println(day + " " + date + " " + month + " " + year);
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+        String day = "Wednesday";
+        int date = 5;
+        String month = "February";
+        int year = 2025;
+
+        System.out.println("American format:");
+        printAmerican(day, date, month, year);
+
+        System.out.println("European format:");
+        printEuropean(day, date, month, year);
     }
 }
